50cbec15b53f428ca1814cb6f385d924f50f56e8
cwbishop <chriswbishop@gmail.com>
2013-10-16 13:36:11 -0700

    Bug fixes prior to Experiment01

--- FFR/code/make_AM_FDL_tones_2freq_4FFR.m
+++ FFR/code/make_AM_FDL_tones_2freq_4FFR.m
@@ -1,48 +1,81 @@
-function [tone_final] = make_AM_FDL_tones_2freq_4FFR(fs, carrier_freq1, carrier_freq2, calibrate, save_wav)
-% This code will make amplitude-modulated tones of 4 second duration with
-% a change in frequency at the 2 second midpoint. These stimuli will
-% tentatively be used in a Frequency Discrimination Psychophysical Training
-% Task.
+function [tone_final] = make_AM_FDL_tones_2freq_4FFR(fs, carrier_freq1, carrier_freq2, calibrate, save_wav, mod_rate, duration, DeBug, AM_TYPE)
+%% DESCRIPTION:
 %
-% This code was originally created for Project E - interaural phase differences
-% in Clinard's JMU lab. This follows the methods of Grose and
-% Mamo, 2010.
+%   Generate an amplitude-modulated (AM) tone of arbitrary duration with a
+%   change in carrier frequency at the midpoint. The code was originally
+%   created for Project E - interaural phase differences in Clinard's JMU
+%   lab. This follows the methods of Grose and Mamo (2010). 
 %
-  % Make AM tone (formula from Picton et al, 2003, IJA ASSR Review and
-  % Dimitrijevic et al, 2001, Human ASSRs to tones Independently Modulated
-  % in both Frequency and Amplitude)
-  % per Grose and Mamo 2010, 3pi/2 starting phase begins and ends at zero.
+%   Make AM tone (formula from Picton et al, 2003, IJA ASSR Review and
+%   Dimitrijevic et al, 2001, Human ASSRs to tones Independently Modulated
+%   in both Frequency and Amplitude)
+%   per Grose and Mamo 2010, 3pi/2 starting phase begins and ends at zero.
 %
-%   where fs is the sampling frequency, 
-%   carrier_freq1 and carrier_freq2 are the first and second carrier freqs.
-%   calibrate == 0 makes of of duration (defined below), == 1 makes
-%   duration of 10 seconds for easier calibration on Sound Level Meter.
-%   save_wav == 0 does NOT save a .wav file, == 1 saves a .wav file
+%% INPUT:
 %
-% Useage example:
+%   fs: sampling rate (default 44100)
+%   carrier_freq1: carrier frequency for first half of stimulus (Hz)
+%                 (no default)
+%   carrier freq2: carrier frequency for second half of stimulus (Hz)
+%                 (no default)
+%   calibrate:     bool, flag set if output calibration required (default
+%                  false)
+%   save_wav:   bool, flag to save the wav file or not. (default false)
+%   mod_rate:   modulation rate in Hz (default 40 Hz); 
+%   duration:   stimulus duration in seconds (default 4 s)
+%   DeBug:      bool, flag to enter debug mode (default false)
+%   AM_TYPE:    string, defines type of amplitude modulation to use.
+%               Options: 
+%                   'SIN':  default option, sinusoidally modulated tones.
+%                   'COS2': cos^2 onset ramps leading into full amplitude
+%                           tones. The hope here is that we can maximize
+%                           the number of full-amplitude cycles we present
+%                           in each burst of the AM signal. %% NOT YET
+%                           IMPLEMENTED %%
+%
+%% OUTPUT:
+%
+%   tone_final: concatenated, AM tone with two carrier frequencies (change
+%               in middle of stimulus)
+%
+%% EXAMPLE:
+%
 % [tone_final] = make_AM_FDL_tones_2freq_4FFR(20000, 1000, 990, 0, 1)
 %
 %  Initially written by C. Clinard ~ Sept. 2013.
-%% Check for Debug and Define Variables
-DeBug = 0; % 1 == plot stimulus, 0 == don't plot stimulus
+%
+%   131014 CWB: Minor edits and reorganization to include mod_freq,
+%   duration, and debug as input variables with default values. 
 
-if DeBug == 1 % This loop is for code development purposes only.
-  carrier_freq1 = 500.00000;            % carrier frequency
-  carrier_freq2 = 450.00000;            % carrier frequency
-  fs = 10000;  % 10 kHz sampling rate matches FFR sampling rate
-end
+%% DEFAULT INPUTS
+if ~exist('mod_rate', 'var'), mod_rate=40; end % default mod rate of 40 Hz
+if ~exist('DeBug', 'var'), DeBug=false; end % default DeBug to 0 (do not plot stimulus)
+if ~exist('duration', 'var'), duration=4.0; end % default duration to 4 (s)
+if ~exist('fs', 'var'), fs=44100; end % default to 44100 sampling rate.
+if ~exist('calibrate', 'var'), calibrate=false; end 
 
-  %%%%%---- Carrier and Modulation  Frequencies should be specified with Coherent Sampling
+%% 131014 CWB:
+%   Commented this out because I wanted the debug functions to work with
+%   whatever input I specify. They're handy functions. 
+%
+% if DeBug == true % This loop is for code development purposes only.
+%   warning(['Entering debug mode: carrier frequencies and sampling rate ' ... 
+%         'may have been overwritten.']); 
+%   carrier_freq1 = 500.00000;            % carrier frequency
+%   carrier_freq2 = 200.00000;            % carrier frequency
+%   fs = 10000;  % 10 kHz sampling rate matches FFR sampling rate
+% end
+
+%%%%%---- Carrier and Modulation  Frequencies should be specified with Coherent Sampling
 Fc1 = carrier_freq1; % Initial frequency over first half of stimulus
 Fc2 = carrier_freq2; %         frequency over the 2nd half of stimulus
-mod_rate = 40; % amplitude modulation rate. 1/40 Hz = 0.025 second period
-duration = 4.0;  % Entire stimulus duration, in seconds.
 
 %% If calibrating, make tone duration long
-if calibrate == 1;          duration = 10.0;              end
+if calibrate == true;          duration = 10.0;              end
 
 %% Make tones
-t = 0:1/fs:duration/2; % time vector, make 2 tones of 2 sec duration then concatenate.
+% 131016 CWB: adjusted t to remove additional sample. 
+t = 0:1/fs:duration/2 - 1/fs; % time vector, make 2 tones of 2 sec duration then concatenate.
 
  % Make Amplitude-Modulated, fixed-phase tone; multiply tone by AM envelope
    % make First frequency AM tone
@@ -53,7 +86,7 @@ tone2 = (.9 * (1 + sin(2*pi*mod_rate*t +(3*pi/2))) .* sin(2*pi*Fc2*t))/2;
 tone_final = [tone1 tone2];  % concatenate tones
 
 %% Plot stimulus if debugging: waveforms and FFTs
-if DeBug == 1;
+if DeBug == true;
     % Plot the stimulus  -----------------------------------------------
     figure
      % Plot stimulus waveform
@@ -79,9 +112,11 @@ if DeBug == 1;
     title('Midpoint of stimulus')
 %     plot(t(index:index*2), stim2chan(2,index:index*2), 'r')
 %     xlim([2-index/fs 2+(index*2)/fs])
-    xlim([2-index/fs 2+index/fs]); % scale to show midpoint +/- one AM cycle
-
-    zoom xon
+    %% 131016CWB: Changed xlim call to zoom relative to duration rather 
+    %  than hardcoded assuming a 4 s stim.
+    xlim([duration/2-index/fs duration/2+index/fs]); % scale to show midpoint +/- one AM cycle
+    
+%     zoom xon
     
          % plot offset of stimulus
     % subplot(2,2,3)
@@ -110,7 +145,7 @@ end
 if save_wav == 1       %%% Create filename and Save a .wav file
 
         % e.g., 'ProjU_500_450Hz_40HzSAM_4sec_10kHz.wav'
-    filename = ['ProjU' num2str(Fc1) '_' num2str(Fc2) 'Hz_' num2str(mod_rate) 'HzSAM' num2str(duration) 'sec_' num2str(fs/1000) 'kHz'];
+    filename = ['../stims/' 'ProjU' num2str(Fc1) '_' num2str(Fc2) 'Hz_' num2str(mod_rate) 'HzSAM' num2str(duration) 'sec_' num2str(fs/1000) 'kHz.wav'];
     % filename = 'ProjU_500_2_450Hz_40HzSAM_4sec_10kHzfs';
     wavwrite(tone_final, fs, filename);         % Save .wav file
 end
