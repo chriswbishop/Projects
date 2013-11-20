9c34bf0a363b68e8dee2b7ec7e403cf2ca5b927f
cwbishop <chriswbishop@gmail.com>
2013-11-20 15:51:39 -0800

    AA02: updated circuit and M-file

--- FFR/code/AA02_Paradigm.m
+++ FFR/code/AA02_Paradigm.m
@@ -1,4 +1,4 @@
-function AA02_Paradigm(CIRCUIT)
+function [RP, RECDATA]=AA02_Paradigm(TIMING_TEST, CIRCUIT)
 %% DESCRIPTION:
 %
 %   Paradigm for Project AA Experiment 02. ALL inputs are optional, except
@@ -10,6 +10,8 @@ function AA02_Paradigm(CIRCUIT)
 %               relative path ../RP_Files/RCX_FILE, which assumes you're
 %               currently in the code directory of the project. If this
 %               isn't true, you're in the wrong directory anyway ;). 
+%   TIMING_TEST:    bool, set to true if you'd like to conduct a timing
+%                   test. (default=false)
 %
 % OUTPUT:
 %
@@ -21,13 +23,18 @@ function AA02_Paradigm(CIRCUIT)
 
 %% DEFAULTS
 
-% TDT Sampling Rate (~100 kHz)
-FSLEVEL=4; % 100kHz
+% TDT Sampling Rate (~50 kHz)
+FSLEVEL=3; % 50 kHz; Any faster than this causes distortions when trying to play/record simultaneously.
 
 %   Circuit default
 if ~exist('CIRCUIT', 'var') || isempty(CIRCUIT)
     CIRCUIT=fullfile('..', 'RP_Files', 'AA02.rcx'); 
 end % 
+
+% Timing Test?
+if ~exist('TIMING_TEST', 'var') || isempty(TIMING_TEST)
+    TIMING_TEST=FALSE;
+end % ~exist('TIMING...
 
 %% ESTABLISH CONNECTION WITH RP 2.1
 RP = actxcontrol('RPco.x',[5 5 26 26]);
@@ -45,8 +52,9 @@ FS = RP.GetSFreq;
 %% MAKE STIMULI
 %   This is a place holder for now. Will need to make the correct stimuli
 %   later. 
-DATA=make_AM_FDL_tones_2freq_4FFR(FS, 1000, 950, false, false, 20, 1, false);
-% DATA=[ones(ceil(FS*0.0005),1); zeros(ceil(FS*1),1)]; 
+DATA=sin_gen(1000,1,FS,0)';
+% DATA=make_AM_FDL_tones_2freq_4FFR(FS, 1000, 1000, false, false, 20, 1, false);
+% DATA=[zeros(ceil(FS*0.05),1); ones(ceil(FS*0.0005),1); zeros(ceil(FS*0.05),1)]; 
 % DATA=[DATA; DATA; DATA; DATA];
 
 %% SET BUFFER LENGTH
@@ -59,7 +67,7 @@ RP.Halt;
 RP.SetTagVal('Trig', 255); % 255 for testing
 
 %% SET TRIGGER DELAY
-RP.SetTagVal('TrigDelay', 0); % 1 sec delay for testing
+RP.SetTagVal('TrigDelay', 0); % trigger delay
 
 %% FEED IN USER SPECIFIED PARAMTERS
 RP.WriteTagV('Snd', 0, DATA); 
@@ -68,4 +76,24 @@ RP.WriteTagV('Snd', 0, DATA);
 RP.Run;
 
 %% PLAY SOUND
-RP.SoftTrg(1); 
+if ~TIMING_TEST
+    RP.SoftTrg(1); 
+elseif TIMING_TEST
+    
+    % Acquire first 10 ms
+    bufpts=ceil(0.01*FS);
+    RECDATA=nan(2,bufpts,2); 
+    
+    % 
+    for i=1:20
+        RP.SoftTrg(1);
+        
+        % Wait until buffer fills with sound    
+        while(RP.GetTagVal('index')<bufpts), end
+    
+        % Pull data out of buffer
+        RECDATA(:,:,i)=RP.ReadTagVEX('dataout', 0, bufpts,'I16','F64',2);       
+        RP.SoftTrg(2);
+        pause(1); 
+    end % i=1:...
+end % if ~TIMING_TEST
