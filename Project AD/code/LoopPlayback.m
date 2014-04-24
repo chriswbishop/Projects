6a52a893b108c9d03308d84244ce3e2cf8f78da2
cwbishop <chriswbishop@gmail.com>
2014-04-24 13:15:17 -0700

    Sound playback loop in psychotoolbox

---
+++ Project AD/code/LoopPlayback.m
@@ -0,0 +1,69 @@
+function LoopPlayback(X, Y, varargin)
+
+[X, FS]=AA_loaddata(X); 
+[Y, FS]=AA_loaddata(Y);
+
+InitializePsychSound;
+portaudio = PsychPortAudio('Open', [], [], 0, FS, size(X,2)); 
+
+% Divide X into chunks
+%   Load it into the buffer in chunks
+nblocks=4;
+blocksize=ceil(size(X,1)/nblocks); %
+disp(num2str(blocksize/FS)); 
+% nblocks=ceil(size(X,1)/blocksize); 
+
+% Fill sound buffer with first two blocks
+PsychPortAudio('FillBuffer', portaudio, X(1:blocksize*2,:)');
+
+% Fill buffer as we move through
+for i=3:nblocks
+    
+    % Start two-channel audio playback
+    PsychPortAudio('Start', portaudio, 2, 0, 1, [], 1);  
+    
+    % Wait for block to finish
+    status=PsychPortAudio('GetStatus', portaudio);
+    while status.PositionSecs<blocksize/FS
+        status=PsychPortAudio('GetStatus', portaudio);
+    end % Loop for a bit
+    
+    % If it's even, replace the second half of buffer
+    %   If it's odd, replace the first half of buffer
+    if mod(i,2)==0
+        sindex=1;
+       % For even sounds, replace the first half of buffer
+%        PsychPortAudio('FillBuffer', portaudio, X(1+(i-1)*blocksize:i*blocksize,:)', 1, 1);
+    else
+        sindex=1+blocksize*2;
+%        PsychPortAudio('FillBuffer', portaudio, X(1+(i-1)*blocksize:i*blocksize,:)', 1, 1+blocksize*2);
+    end % 
+    
+    if i==nblocks 
+        PsychPortAudio('FillBuffer', portaudio, X(1+(i-1)*blocksize:size(X,1),:)', 1, sindex);
+    else
+        PsychPortAudio('FillBuffer', portaudio, X(1+(i-1)*blocksize:i*blocksize,:)', 1, sindex);
+    end % if i==nblocks
+    
+end % for i=3:nblocks
+
+% for i=1:nblocks
+       
+%     if i==1
+%         PsychPortAudio('FillBuffer', portaudio, zeros(size(X))');
+%         PsychPortAudio('FillBuffer', portaudio, X(1+blocksize*(i-1):blocksize*i,:)');
+%         PsychPortAudio('FillBuffer', portaudio, X(1:blocksize*(nblocks-1))');
+%         PsychPortAudio('Start', portaudio, 2, 0, 1, [], 1);  
+%   
+%         status=PsychPortAudio('GetStatus', portaudio);
+%         while status.PositionSecs<blocksize/FS
+%             status=PsychPortAudio('GetStatus', portaudio);
+%         end % Loop for a bit
+%         
+%         PsychPortAudio('FillBuffer', portaudio, Y', 1);
+% %     end % if i==1:
+%     
+% % end % for i=1:nblocks
+% 
+% disp('Blah');
+%         
